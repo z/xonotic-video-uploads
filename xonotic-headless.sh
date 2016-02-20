2a3d1b0d21799dae89350766e714d6e11a77d70b
z <z@xnz.me>
2016-02-20 16:43:59 -0500

    this extension may be needed

--- xonotic-headless.sh
+++ xonotic-headless.sh
@@ -11,20 +11,22 @@ cd ${ENGINE_PATH}
 
 if [[ "$1" == "-w" ]]; then
 
-    echo xinit ./${ENGINE_BIN} run -simsound -sessionid client -userdir "$USER_DIR" +playdemo "${RELATIVE_WORKING_DIR}sliced/$2" -- /usr/bin/Xvfb :1 -screen 0 1920x1080x16
+    echo xinit ./${ENGINE_BIN} run -simsound -sessionid client -userdir "$USER_DIR" +playdemo "${RELATIVE_WORKING_DIR}sliced/$2" -- /usr/bin/Xvfb :1 -screen 0 1920x1080x16 +extension RENDER
 
-    xinit ./${ENGINE_BIN} run -simsound -sessionid client -userdir "$USER_DIR" +playdemo "${RELATIVE_WORKING_DIR}sliced/$2" -- /usr/bin/Xvfb :1 -screen 0 1920x1080x16
+    xinit ./${ENGINE_BIN} run -simsound -sessionid client -userdir "$USER_DIR" +playdemo "${RELATIVE_WORKING_DIR}sliced/$2" -- /usr/bin/Xvfb :1 -screen 0 1920x1080x16 +extension RENDER
+
+    cd -
 
 elif [[ "$1" == "-d" ]]; then
 
-    echo xinit ./${ENGINE_BIN} run -simsound -sessionid client -userdir "$USER_DIR" +playdemo "$2" -- /usr/bin/Xvfb :1 -screen 0 1920x1080x16
+    echo xinit ./${ENGINE_BIN} run -simsound -sessionid client -userdir "$USER_DIR" +playdemo "$2" -- /usr/bin/Xvfb :1 -screen 0 1920x1080x16 +extension RENDER
 
-    xinit ./${ENGINE_BIN} run -simsound -sessionid client -userdir "$USER_DIR" +playdemo "$2" -- /usr/bin/Xvfb :1 -screen 0 1920x1080x16
+    xinit ./${ENGINE_BIN} run -simsound -sessionid client -userdir "$USER_DIR" +playdemo "$2" -- /usr/bin/Xvfb :1 -screen 0 1920x1080x16 +extension RENDER
+
+    cd -
 
 else
     echo "No parameter. Use either -w or -d"
     cd -
     exit 1
 fi
-
-cd -
